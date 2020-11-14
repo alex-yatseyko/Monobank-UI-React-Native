4028e9a2e07664d77c986213a5e3207a1be46822
Oleksandr <oleksandr.k@youpal.es>
2020-11-15 00:58:56 +0200

    Flastlist Carousel-1

--- App.tsx
+++ App.tsx
@@ -1,15 +1,153 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { EvilIcons } from "@expo/vector-icons";
+import {
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from "react-native-gesture-handler";
+
+const { width } = Dimensions.get("screen");
+const DATA = [
+  {
+    title: "Afro vibes",
+    location: "Mumbai, India",
+    date: "Nov 17th, 2020",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2020/07/Afro-vibes-flyer-template.jpg",
+  },
+  {
+    title: "Jungle Party",
+    location: "Unknown",
+    date: "Sept 3rd, 2020",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2019/11/Jungle-Party-Flyer-Template-1.jpg",
+  },
+  {
+    title: "4th Of July",
+    location: "New York, USA",
+    date: "Oct 11th, 2020",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2020/06/4th-Of-July-Invitation.jpg",
+  },
+  {
+    title: "Summer festival",
+    location: "Bucharest, Romania",
+    date: "Aug 17th, 2020",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2020/07/Summer-Music-Festival-Poster.jpg",
+  },
+  {
+    title: "BBQ with friends",
+    location: "Prague, Czech Republic",
+    date: "Sept 11th, 2020",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2020/06/BBQ-Flyer-Psd-Template.jpg",
+  },
+  {
+    title: "Festival music",
+    location: "Berlin, Germany",
+    date: "Apr 21th, 2021",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2020/06/Festival-Music-PSD-Template.jpg",
+  },
+  {
+    title: "Beach House",
+    location: "Liboa, Portugal",
+    date: "Aug 12th, 2020",
+    poster:
+      "https://www.creative-flyers.com/wp-content/uploads/2020/06/Summer-Beach-House-Flyer.jpg",
+  },
+];
+
+const OVERFLOW_HEIGHT = 70;
+const SPACING = 10;
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+const VISIBLE_ITEMS = 3;
+// Advanced React Native FlatList stack carousel animations at 60fps
+
+const OverflowItems = ({ data }) => {
+  return (
+    <View style={styles.overflowContainer}>
+      <View>
+        {data.map((item, index) => {
+          return (
+            <View key={index}>
+              <Text style={[styles.title]} numberOfLines={1}>
+                {item.title}
+              </Text>
+              <View style={styles.itemContainerRow}>
+                <Text style={[styles.location]}>
+                  <EvilIcons
+                    name="location"
+                    size={16}
+                    color="black"
+                    style={{ marginRight: 5 }}
+                  />
+                  {item.location}
+                </Text>
+                <Text style={[styles.date]}>{item.date}</Text>
+              </View>
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
-  const [fadeValue, setFadeValue] = React.useState(new Animated.Value(0))
- 
+  const [data, setData] = React.useState(DATA);
+  const scrollXIndex = React.useRef(new Animated.Value(0)).current;
+  const scrollXAnimated = React.useRef(new Animated.Value(0)).current;
+
   return (
     <View style={styles.container}>
-      {/* <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
-      <Animated.View style={styles.animationView}></Animated.View>
+      <OverflowItems data={DATA} />
+      <FlatList
+        data={data}
+        keyExtractor={(_, index) => String(index)}
+        horizontal
+        inverted
+        scrollEnabled={false}
+        removeClippedSubviews={false}
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          paddingHorizontal: SPACING * 2
+        }}
+        CellRendererComponent={({item, index, children, style, ...props}) => {
+          const newStyle = [
+            style,
+            { zIndex: data.length - index }
+          ]
+          return(<View style={newStyle} index={index} {...props}>{children}</View>)
+        }}
+        renderItem={({item, index}) => {
+          return (
+            <View style={{position: 'absolute', left: -ITEM_WIDTH / 2}}>
+              <Image
+                source={{ uri: item.poster }}
+                style={{
+                  width: ITEM_WIDTH,
+                  height: ITEM_HEIGHT,
+                  borderRadius: 14,
+                }}
+              />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -17,13 +155,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    backgroundColor: "#fff",
   },
-  animationView: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'skyblue'
-  }
+  title: {
+    fontSize: 28,
+    fontWeight: "900",
+    textTransform: "uppercase",
+    letterSpacing: -1,
+  },
+  location: {
+    fontSize: 16,
+  },
+  date: {
+    fontSize: 12,
+  },
+  itemContainer: {
+    height: OVERFLOW_HEIGHT,
+    padding: SPACING * 2,
+  },
+  itemContainerRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  overflowContainer: {
+    height: OVERFLOW_HEIGHT,
+    overflow: "hidden",
+  },
 });
